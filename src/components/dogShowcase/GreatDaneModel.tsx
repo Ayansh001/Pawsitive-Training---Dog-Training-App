888346ee9f29918e14cceee5b626a7740b783c8b
Ayansh001 <shivanshusinghrajput666@gmail.com>
2025-04-30 14:39:02 +0530

    Update GreatDaneModel.tsx

--- src/components/dogShowcase/GreatDaneModel.tsx
+++ src/components/dogShowcase/GreatDaneModel.tsx
@@ -1,5 +1,5 @@
 
-import React from 'react';
+  import React from 'react';
 
 const GreatDaneModel: React.FC = () => {
   return (
@@ -10,12 +10,19 @@ const GreatDaneModel: React.FC = () => {
           frameBorder="0" 
           allowFullScreen={true}
           allow="autoplay; fullscreen; xr-spatial-tracking" 
-          src="https://sketchfab.com/models/c69afc6664da48b9b1d9ab476d8c6dbd/embed?ui_theme=dark&dnt=1"
+          src="https://sketchfab.com/models/c69afc6664da48b9b1d9ab476d8c6dbd/embed?autospin=1&autostart=1&transparent=1"
           className="w-full h-full rounded-lg"
+          // Using data attributes for non-standard properties
+          data-mozallowfullscreen="true"
+          data-webkitallowfullscreen="true"
+          data-xr-spatial-tracking=""
+          data-execution-while-out-of-viewport=""
+          data-execution-while-not-rendered=""
+          data-web-share=""
         />
       </div>
     </div>
   );
 };
 
-export default GreatDaneModel;
+
